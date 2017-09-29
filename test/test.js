1a33a5ab9172b0534161bf4c59260e1f6f1ff839
ratajczak <piotr.ratajczak@recoded.co>
2017-09-29 13:07:07 +0200

    empty tests

---
+++ test/test.js
@@ -0,0 +1,33 @@
+const assert = require('assert');
+
+describe('jsonNotationSwitcher', () => {
+
+	describe("switch camelCase to '_'", ()=>{
+
+	});
+
+	describe("switch camelCase to custom delimiter notation", ()=>{
+
+	});
+
+	describe("switch '_' to camelCase", ()=>{
+
+	});
+
+	describe("switch '_' to custom notation", ()=>{
+
+	});
+
+	describe("switch custom notation to camelCase", ()=>{
+
+	});
+
+	describe("switch custom notation to  '_'", ()=>{
+
+	});
+
+	describe("switch custom notation to another custom notation", ()=>{
+
+	});
+
+})
